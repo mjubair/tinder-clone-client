8bdb44ba47fde52c2053850e7058168c53d3672a
Mohamed Jubair <amjubair@outlook.com>
2020-10-26 22:39:52 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tinder Clone - FrontEnd
+
+Tinder clone reactjs application
+
+[Check out the Application](https://codesandbox.io/s/heuristic-fire-nu3gx)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* tinder swipe secion */}
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
